e0d3b21fa427c8088b1bd6a7bbb1d6b26c889657
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-23 09:38:48 +0300

    8gb of zram... uhhh

--- bin/bin/zram.sh
+++ bin/bin/zram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 sudo modprobe zram
 echo "lz4" | sudo tee /sys/block/zram0/comp_algorithm
-echo "$(echo '2 * 1024 * 1024 * 1024' | bc)" | sudo tee /sys/block/zram0/disksize
+echo "$(echo '8 * 1024 * 1024 * 1024' | bc)" | sudo tee /sys/block/zram0/disksize
 sudo mkswap /dev/zram0
 sudo swapon /dev/zram0
