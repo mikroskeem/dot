0b514539b43a91e86038c6f1c6fb400e9653cafc
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-23 09:39:10 +0300

    teh ultimeit pauwersave skriptt

---
+++ bin/bin/clamp.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+find_powerclamp () {
+    find /sys/class/thermal -name "cooling_device*" | while read _p; do
+        if (cat "${_p}/type" | grep -q "intel_powerclamp"); then
+            echo "${_p}"
+            break
+        fi
+    done
+}
+
+clamp_dev="`find_powerclamp`"
+
+if [ ! -z "${clamp_dev}" ]; then
+    sudo tee "${clamp_dev}/cur_state" <<<${1}
+fi
