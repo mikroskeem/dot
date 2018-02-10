3603c503eaa3329469fd387b1b415272dc270dcf
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-02-11 00:35:35 +0200

    Add utility to toggle between NVIDIA and Intel graphics card

---
+++ bin/bin/toggle-nvidia.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+FILE="/usr/share/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf"
+
+# Update when it gets updated in 'nvidia-utils' package
+CONFIG='
+Section "OutputClass"
+    Identifier "intel"
+    MatchDriver "i915"
+    Driver "modesetting"
+EndSection
+
+Section "OutputClass"
+    Identifier "nvidia"
+    MatchDriver "nvidia-drm"
+    Driver "nvidia"
+    Option "AllowEmptyInitialConfiguration"
+    Option "PrimaryGPU" "yes"
+    ModulePath "/usr/lib/nvidia/xorg"
+    ModulePath "/usr/lib/xorg/modules"
+EndSection
+'
+
+if ! readlink "${FILE}" 1>/dev/null ; then
+    sudo ln -sf /dev/null "${FILE}"
+else
+    sudo rm "${FILE}"
+    echo -en "$CONFIG" | sudo tee "${FILE}" 1> /dev/null
+fi
+
+echo "Don't forget to restart your Xorg session!"
